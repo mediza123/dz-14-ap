617edf92de7fd69047e6ef6f977d47423f4110f7
mediza123 <artem.karapetian6533@yandex.ru>
2025-04-13 23:17:51 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+def prime_generator():
+    """Генератор простых чисел"""
+    num = 2
+    while True:
+        is_prime = True
+        
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield num
+        num += 1
